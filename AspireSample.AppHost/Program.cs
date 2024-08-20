3974273a7d6c6c0bfe341b4cde5979f196174bf7
Roger! <mahjong.mahjong@gmail.com>
2024-08-20 15:37:38 -0700

    feat: initial commit for a learning aspire project

---
+++ AspireSample.AppHost/Program.cs
@@ -0,0 +1,27 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var postgres = builder.AddPostgres("postgres")
+                      .PublishAsAzurePostgresFlexibleServer();
+
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
+
+/*var weatherApi = builder.AddProject<Projects.AspireJavaScript_MinimalApi>("weatherapi")
+    .WithExternalHttpEndpoints();*/
+
+builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WithReference(apiService);
+
+builder.AddNpmApp("reactmahjong", "../reactmahjong")
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+//.WithReference(weatherApi)
+
+builder.Build().Run();
